6bf5acadb38b1a8d9f1a32d6ae0bc5b89d3c1fe9
MGDACHI <dachidzagnidze2009@gmail.com>
2024-12-14 14:25:28 +0400

    second commit

--- Level 60/Homework/main.py
+++ Level 60/Homework/main.py
@@ -1,55 +1,14 @@
 
-#! reverse code
-def reverse_words(text):
-    result = []
-    word = []
 
-    for char in text:
-        if char == " ":
-            
-            for i in range(len(word) - 1, -1, -1):
-                result.append(word[i])
-                
-            word = []  
-            result.append(" ") 
-        else:
-            word.append(char)
-            
-    for i in range(len(word) - 1, -1, -1):
-        result.append(word[i])
-    output = ""
-    
-    for char in result:
-        output += char
-
-    return output
-
-
-    #! second way to slove 
+#reverse
 
 def reverse_words(text):
+    text = text.split(" ")
     result = []
-    word = ""
-    
-    for i in range(len(text)):
-        
-        if text[i] == " ":
-            
-            if word:
-                result.append(word[::-1])
-                word = ""
-                
-            result.append(" ")
-        else:
-            word += text[i]
-    
-    if word:
+    for word in text:
         result.append(word[::-1])
     
-    return ''.join(result)
-
-
-
+    return " ".join(result)
 
 
 
@@ -57,31 +16,22 @@ def reverse_words(text):
 
 def draw_stairs(n):
     result = []
-    
     for i in range(n):
-        step = ' ' * i + 'I'  
+        step = " " * i + "I"
 
         result.append(step)
-    return '\n'.join(result)
-
-
-
+    return '\n'. join(result)
+    
 
 #! matrix code
 
 
 def get_matrix(n):
-    matrix = []
-    #რიგი
+    result = []
     for i in range(n):
-        row = []  
-        #სვეტი
-        for j in range(n):
-            if i == j: 
-                row.append(1)
-            else:  
-                row.append(0)
-        matrix.append(row) 
-    
-    return matrix
-    
+        inner = []
+        for j in range(n - 1):
+            inner.append(0)
+        inner.insert(i, 1)
+        result.append(inner)
+    return result
